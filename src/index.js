bf1c2ef0c411bbcef5fef4e86a6aa8ac8ed3cf6e
DiniHasanah2 <dinihsnh22@gmail.com>
2025-02-03 02:33:22 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { defaultProps, targetElements } from "./data/scrollRevealConfig";
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
